c73635ef252f71efb0ac15d1edb3bc7042f3e6a6
Jorge Cortés <jorgeacortes@users.noreply.github.com>
2023-05-24 16:59:32 +0200

    Implement move status and retry button

--- app_html/lib/app_html_web/controllers/hangman.ex
+++ app_html/lib/app_html_web/controllers/hangman.ex
@@ -4,7 +4,46 @@ defmodule AppHtmlWeb.HangmanHTML do
   import Phoenix.HTML.Link
   import Phoenix.HTML.Form
 
+  alias AppHtmlWeb.Router.Helpers, as: Routes
+
   embed_templates "hangman/*"
+
+  def continue_or_try_again(conn, status) when status in [:won, :lost] do
+    button("Try again",
+      to: Routes.hangman_path(conn, :new),
+      class: "flex mt-2 mx-auto bg-brand text-white font-semibold p-2"
+    )
+  end
+
+  def continue_or_try_again(conn, _) do
+    form_for(
+      conn,
+      Routes.hangman_path(conn, :update),
+      [as: "make_move", method: :put],
+      fn f ->
+        [
+          text_input(f, :guess),
+          submit("Make the next guess",
+            class: "flex mt-2 mx-auto bg-brand text-white font-semibold p-2"
+          )
+        ]
+      end
+    )
+  end
+
+  @status_fields %{
+    initializing: {"text-black", "Guess the word, a letter a a time"},
+    good_guess: {"text-green-400", "Good guess!"},
+    bad_guess: {"text-red-400", "Sorry, that's a bad guess"},
+    won: {"text-brand", "You won!"},
+    lost: {"text-purple-400", "Sorry, you lost"},
+    already_used: {"text-gray-800", "You already used that letter"}
+  }
+
+  def move_status(status) do
+    {class, msg} = @status_fields[status]
+    "<div class='font-semibold #{class}'>#{msg}</div>"
+  end
 
   def figure_for(0) do
     ~s{
