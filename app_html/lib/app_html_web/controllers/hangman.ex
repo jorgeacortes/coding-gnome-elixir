caf83a641e06ab9aaed3b131fd823e91f31b532a
Jorge Cortés <jorgeacortes@users.noreply.github.com>
2023-05-24 16:42:01 +0200

    tidying up and added hangman _graphics_

--- app_html/lib/app_html_web/controllers/hangman.ex
+++ app_html/lib/app_html_web/controllers/hangman.ex
@@ -5,4 +5,100 @@ defmodule AppHtmlWeb.HangmanHTML do
   import Phoenix.HTML.Form
 
   embed_templates "hangman/*"
+
+  def figure_for(0) do
+    ~s{
+      ┌───┐
+      │   │
+      O   │
+     /|\\  │
+     / \\  │
+          │
+    ══════╧══
+    }
+  end
+
+  def figure_for(1) do
+    ~s{
+      ┌───┐
+      │   │
+      O   │
+     /|\\  │
+     /    │
+          │
+    ══════╧══
+    }
+  end
+
+  def figure_for(2) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+   /|\\  │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(3) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+   /|   │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(4) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+    |   │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(5) do
+    ~s{
+    ┌───┐
+    │   │
+    O   │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(6) do
+    ~s{
+    ┌───┐
+    │   │
+        │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
+
+  def figure_for(7) do
+    ~s{
+    ┌───┐
+        │
+        │
+        │
+        │
+        │
+  ══════╧══
+}
+  end
 end
--- app_html/lib/app_html_web/controllers/hangman_controller.ex
+++ app_html/lib/app_html_web/controllers/hangman_controller.ex
@@ -1,5 +1,6 @@
 defmodule AppHtmlWeb.HangmanController do
   use AppHtmlWeb, :controller
+  alias AppHtmlWeb.Router.Helpers, as: Routes
 
   def index(conn, _params) do
     render(conn, :index)
@@ -7,22 +8,28 @@ defmodule AppHtmlWeb.HangmanController do
 
   def new(conn, _params) do
     game = Hangman.new_game()
-    tally = Hangman.tally(game)
 
     conn
     |> put_session(:game, game)
-    |> render("game.html", tally: tally)
+    |> redirect(to: Routes.hangman_path(conn, :show))
   end
 
   def update(conn, params) do
     guess = params["make_move"]["guess"]
 
+    put_in(conn.params["make_move"]["guess"], "")
+    |> get_session(:game)
+    |> Hangman.make_move(guess)
+
+    redirect(conn, to: Routes.hangman_path(conn, :show))
+  end
+
+  def show(conn, _param) do
     tally =
       conn
       |> get_session(:game)
-      |> Hangman.make_move(guess)
+      |> Hangman.tally()
 
-    put_in(conn.params["make_move"]["guess"], "")
-    |> render("game.html", tally: tally)
+    render(conn, "game.html", tally: tally)
   end
 end
