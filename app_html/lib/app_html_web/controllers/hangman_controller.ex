6ecfb90b0435a2e5a39bd97182e4bfd071498b5d
Jorge Cortés <jorgeacortes@users.noreply.github.com>
2023-05-24 16:20:29 +0200

    minor design update

--- app_html/lib/app_html_web/controllers/hangman_controller.ex
+++ app_html/lib/app_html_web/controllers/hangman_controller.ex
@@ -2,7 +2,7 @@ defmodule AppHtmlWeb.HangmanController do
   use AppHtmlWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index, layout: false)
+    render(conn, :index)
   end
 
   def new(conn, _params) do
