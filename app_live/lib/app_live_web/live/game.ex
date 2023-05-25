076654fb9b4b1cf2c1bdbc4504ad6169ab46ace9
Jorge Cortés <jorgeacortes@users.noreply.github.com>
2023-05-25 10:20:59 +0200

    word so far live component

--- app_live/lib/app_live_web/live/game.ex
+++ app_live/lib/app_live_web/live/game.ex
@@ -12,10 +12,15 @@ defmodule AppLiveWeb.Live.Game do
     {:ok, socket}
   end
 
+  def handle_event("make_move", %{"key" => key}, socket) do
+    tally = Hangman.make_move(socket.assigns.game, key)
+    {:noreply, assign(socket, %{tally: tally})}
+  end
+
   def render(assigns) do
     ~H"""
     <h1>LiveView Hangman</h1>
-    <div>
+    <div class="" phx-window-keyup="make_move">
       <%= live_component(__MODULE__.Figure, tally: assigns.tally, id: 1) %>
       <%= live_component(__MODULE__.Alphabet, tally: assigns.tally, id: 2) %>
       <%= live_component(__MODULE__.WordSoFar, tally: assigns.tally, id: 3) %>
--- app_live/lib/app_live_web/live/game/word_so_far.ex
+++ app_live/lib/app_live_web/live/game/word_so_far.ex
@@ -1,13 +1,46 @@
 defmodule AppLiveWeb.Live.Game.WordSoFar do
   use AppLiveWeb, :live_component
 
+  @states %{
+    initializing: {"text-black", "Guess the word, a letter a a time"},
+    good_guess: {"text-green-400", "Good guess!"},
+    bad_guess: {"text-red-400", "Sorry, that's a bad guess"},
+    won: {"text-brand", "You won!"},
+    lost: {"text-purple-400", "Sorry, you lost"},
+    already_used: {"text-gray-800", "You already used that letter"}
+  }
+
   def mount(socket) do
     {:ok, socket}
   end
 
+  defp state_name(state) do
+    {_, text} = @states[state] || "Unknown state"
+    text
+  end
+
+  defp state_class(state) do
+    {class, _} = @states[state] || "Unknown state"
+    class
+  end
+
   def render(assigns) do
     ~H"""
-    <p>WordSoFar</p>
+    <div>
+      <% state_class = state_class(@tally.game_state) %>
+      <p class={state_class}>
+        <%= state_name(@tally.game_state) %>
+      </p>
+      <div>
+        <%= for ch <- @tally.letters do %>
+          <% correct = if ch != "_", do: "text-green-400", else: "" %>
+          <% cls = "px-2 text-2xl font-extrabold inline-block " <> correct %>
+          <div class={cls}>
+            <%= ch %>
+          </div>
+        <% end %>
+      </div>
+    </div>
     """
   end
 end
